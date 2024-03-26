8f29534049aad42ab7ca54074c66cb73905e6c0c
Yeabtakele <bereketabe434@gmail.com>
2024-03-26 03:50:56 -0700

    the

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-provider => 'pip3
-  ensure   => '2.1.0',
-  '
+package { 'python3-pip':
+  ensure => installed,
 }
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
+
